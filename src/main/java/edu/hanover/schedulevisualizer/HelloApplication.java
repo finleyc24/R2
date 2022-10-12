59c6163c9f94d9d8b45d56ece181022cf8c1a89e
Haris Skiadas <cskiadas@gmail.com>
2022-10-12 12:53:09 -0400

    Add icon

--- src/main/java/edu/hanover/schedulevisualizer/HelloApplication.java
+++ src/main/java/edu/hanover/schedulevisualizer/HelloApplication.java
@@ -3,6 +3,7 @@ package edu.hanover.schedulevisualizer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("icon.png")));
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
