1ba5405f04fd630b39805a2aa9ee8ed853c8aa5d
Haris Skiadas <cskiadas@gmail.com>
2022-10-18 15:48:35 -0400

    Create TimeSlotGrid view. Ref #1

---
+++ src/main/java/edu/hanover/schedulevisualizer/ui/elements/TimeSlotGrid.java
@@ -0,0 +1,44 @@
+package edu.hanover.schedulevisualizer.ui.elements;
+
+import edu.hanover.schedulevisualizer.core.Weekday;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+
+public class TimeSlotGrid extends HBox {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public TimeSlotGrid() {
+        super();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "timeslotgrid-view.fxml"));
+        fxmlLoader.setRoot(this);
+        addDayColumns();
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void addDayColumns() {
+        for (Weekday weekday : Weekday.values()) {
+            DayColumn dayColumn = new DayColumn();
+            dayColumn.setDay(weekday);
+            dayColumn.setId("column" + weekday);
+            getChildren().add(dayColumn);
+        }
+
+    }
+
+
+}
