612e97bbd6d2db9686ce94975ed7457c3cb3de11
cjhubert03 <hubertc25@hanover.edu>
2022-10-20 14:46:56 -0400

    Began Course Class

--- src/main/java/edu/hanover/schedulevisualizer/core/Course.java
+++ src/main/java/edu/hanover/schedulevisualizer/core/Course.java
@@ -1,5 +1,16 @@
 package edu.hanover.schedulevisualizer.core;
 
 public class Course {
+    public  TimeSlot timeslot1;
+    public  String prefix;
+    public  String courseNum;
+    public  String courseName;
+
+    public Course(TimeSlot timeslot1, String prefix, String courseNum, String courseName){
+        this.timeslot1 = timeslot1;
+        this.prefix = prefix;
+        this.courseNum = courseNum;
+        this.courseName = courseName;
+    }
 
 }
--- src/test/java/edu/hanover/schedulevisualizer/core/CourseTest.java
+++ src/test/java/edu/hanover/schedulevisualizer/core/CourseTest.java
@@ -1,5 +1,23 @@
 package edu.hanover.schedulevisualizer.core;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CourseTest {
+    @Test
+    public void canCreateCourse(){
+        assertCreatedCourseHasCorrectParameters("Data Structures", "223", "CS", new TimeSlot(Weekday.MWF(), 1));
+        assertCreatedCourseHasCorrectParameters("System Fundamentals", "231", "CS", new TimeSlot(Weekday.TR(), 7));
+    }
+
+    private static void assertCreatedCourseHasCorrectParameters(String courseName, String courseNum, String prefix, TimeSlot timeslot1) {
+        Course course1 = new Course(timeslot1, prefix, courseNum, courseName);
+        assertThat(course1.prefix, equalTo(prefix));
+        assertThat(course1.courseNum, equalTo(courseNum));
+        assertThat(course1.courseName, equalTo(courseName));
+        assertThat(course1.timeslot1, equalTo(timeslot1));
+    }
+
 
 }
